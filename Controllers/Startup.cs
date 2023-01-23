873b1a8eea681ff23b8ab7c724a241bd04c0993d
Ruben Fajardo <rfaiard@gmail.com>
2023-01-22 22:01:09 -0600

    publish config & zip

--- Controllers/Startup.cs
+++ Controllers/Startup.cs
@@ -29,7 +29,7 @@ namespace Controllers
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/build";
+                configuration.RootPath = "../ClientApp/build";
             });
         }
 
@@ -66,7 +66,7 @@ namespace Controllers
 
             app.UseSpa(spa =>
             {
-                spa.Options.SourcePath = "ClientApp";
+                spa.Options.SourcePath = "../ClientApp";
 
                 if (env.IsDevelopment())
                 {
